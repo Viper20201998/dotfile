77db6f5b8199d75d15e95f862ba8f9a439fbadf1
Viper20201998 <69060518+Viper20201998@users.noreply.github.com>
2022-01-29 12:28:12 -0600

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,15 +1,2 @@
 #!/bin/sh
 
-# systray battery icon
-#cbatticon -u 5 &
-# systray volume
-volumeicon &
-udiskie -t &
-#nm-applet &
-setxkbmap us &
-feh --bg-scale /home/viper/fondo.jpg &
-picom &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-numlockx on &
-lxqt-powermanagement &
-optimus-manager-qt &
